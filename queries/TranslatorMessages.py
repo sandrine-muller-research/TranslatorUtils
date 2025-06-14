f67950f78203b52d2341d0a87aa8e9e338888c10
sandrine-muller-research <sandrine.muller.research@gmail.com>
2025-06-15 00:13:40 +0200

    Update TranslatorMessages.py

--- queries/TranslatorMessages.py
+++ queries/TranslatorMessages.py
@@ -10,32 +10,32 @@ def pathfinder_message(ids_n0, ids_n2, categories, predicates):
         "message" : {
           "query_graph": {
               "nodes": {
-                  "n0": {
+                  "sn": {
                       "ids": ids_n0
                       },
                   "un": {
                       "categories": categories
                       },
-                  "n2": {
+                  "on": {
                       "ids": ids_n2
                       }
                   },
               "edges": {
                   "e0": {
-                      "subject": "n0",
+                      "subject": "sn",
                       "object": "un",
                       "predicates": [predicates[0]],
                       "knowledge_type": "inferred"
                       },
                   "e1": {
                       "subject": "un",
-                      "object": "n2",
+                      "object": "on",
                       "predicates": [predicates[1]],
                       "knowledge_type": "inferred"
                       },
                   "e2": {
-                      "subject": "n0",
-                      "object": "n2",
+                      "subject": "sn",
+                      "object": "on",
                       "predicates": [predicates[2]],
                       "knowledge_type": "inferred"
                       }
